ca2be1f7a4f93e1dc8de07392d293fb34ab76822
pathumnudda <pathumnudda.jn@gmail.com>
2024-03-28 12:04:17 +0700

    Update MyApp1.py

--- MyApp1.py
+++ MyApp1.py
@@ -19,9 +19,15 @@ cl1.write(dt['sepal.length'].sum())
 cl2.write(dt['sepal.width'].sum())
 cl3.write(dt['petal.length'].sum())
 cl4.write(dt['petal.width'].sum())
-cols=['sepal.length', 'sepal.width', 'petal.length']
-dx=dt[cols]
-st.bar_chart(dx,x='sepal.length',y='sepal.width',color='petal.length')
+
+dx=pd.DataFrame(
+    {
+        "cl1":dt['sepal.length'].sum()
+        "cl2":dt['sepal.width'].sum()
+        "cl3":dt['petal.length'].sum()
+    }
+)
+st.bar_chart(dx,x='col1',y='col2',color='col3')
 
 st.write('ค่าเฉลี่ย')
 cl1,cl2,cl3,cl4=st.columns(4)
