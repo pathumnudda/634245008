99073c677521557dc115101b0e748fb4be60d4bd
pathumnudda <pathumnudda.jn@gmail.com>
2024-03-28 11:46:03 +0700

    Update MyApp1.py

--- MyApp1.py
+++ MyApp1.py
@@ -1,17 +1,23 @@
 import streamlit as st
-import pandas as pd 
-st.title("🌷🌷Website Developing using Python🌷🌷")
+import pandas as pd
+t.title("🌷🌷Website Developing using Python🌷🌷")
 st.header("🎈🎈Website Developing using Python🎈🎈")
 st.subheader("🐬Pathumnudda🐬")
 st.image('aa.jpg')
 
+#Index(['sepal.length', 'sepal.width', 'petal.length', 'petal.width','variety'], dtype='object')
+
 dt=pd.read_csv('./data/iris.csv')
-st.subheader("ข้อมูลดอกไม้ Iris")
+st.subheader("ข้อมูลดอกไม้")
 st.write(dt.head(10))
-#Index(['sepal.length', 'sepal.width', 'petal.length', 'petal.width',
-#       'variety'],
 
-st.subheader("สถิติข้อมูลดอกไม้ Iris")
-
-
- 
+st.subheader("ข้อมูลดอกไม้")
+st.write('ผลรวม')
+cl1,cl2,cl3,cl4=st.columns(4)
+cl1.write(dt['sepal.length'].sum())
+cl2.write(dt['sepal.width'].sum())
+cl3.write(dt['petal.length'].sum())
+cl4.write(dt['petal.width'].sum())
+st.write('ค่าเฉลี่ย')
+st.write('ค่ามากสุด')
+st.write('ค่าน้อยสุด')
