942858655c50f8952f45d2d21e6a9b773236465a
pathumnudda <pathumnudda.jn@gmail.com>
2024-03-28 11:44:45 +0700

    Update MyApp1.py

--- MyApp1.py
+++ MyApp1.py
@@ -1,12 +1,11 @@
 import streamlit as st
 import pandas as pd 
-
 st.title("🌷🌷Website Developing using Python🌷🌷")
 st.header("🎈🎈Website Developing using Python🎈🎈")
 st.subheader("🐬Pathumnudda🐬")
 st.image('aa.jpg')
 
-dt=pd.read_csv('/data/iris.csv')
+dt=pd.read_csv('./data/iris.csv')
 st.subheader("ข้อมูลดอกไม้ Iris")
 st.write(dt.head(10))
 #Index(['sepal.length', 'sepal.width', 'petal.length', 'petal.width',
