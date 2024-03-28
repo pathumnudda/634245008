eefa8c3af131c5101d9ec20745b6265c005534e4
pathumnudda <pathumnudda.jn@gmail.com>
2024-03-28 11:17:23 +0700

    Update MyApp1.py

--- MyApp1.py
+++ MyApp1.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("Website Developing using Python")
 st.header("Website Developing using Python")
-st.subheader("Pathumnudda Juinim")
+st.subheader("Pathumnudda ")
 st.image('aa.jpg')
