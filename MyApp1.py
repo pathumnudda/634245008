28bef7584a4b5b1741785636dca1ffea217854f8
pathumnudda <pathumnudda.jn@gmail.com>
2024-03-28 11:29:19 +0700

    Update MyApp1.py

--- MyApp1.py
+++ MyApp1.py
@@ -1,6 +1,10 @@
 import streamlit as st
+import pandas as pd 
 
 st.title("🌷🌷Website Developing using Python🌷🌷")
 st.header("🎈🎈Website Developing using Python🎈🎈")
 st.subheader("🐬Pathumnudda🐬")
 st.image('aa.jpg')
+
+dt=pd.read_csv('./Data.iris.csv')
+st.write(dt.head(10))
