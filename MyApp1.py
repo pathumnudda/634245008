6d658233226cb5ead0c431f723c344e37d6f3dbb
pathumnudda <pathumnudda.jn@gmail.com>
2024-03-28 11:42:22 +0700

    Update MyApp1.py

--- MyApp1.py
+++ MyApp1.py
@@ -6,10 +6,13 @@ st.header("🎈🎈Website Developing using Python🎈🎈")
 st.subheader("🐬Pathumnudda🐬")
 st.image('aa.jpg')
 
-dt=pd.read_csv('./data/iris.csv')
+dt.pd.read_csv('/data/iris.csv')
 st.subheader("ข้อมูลดอกไม้ Iris")
 st.write(dt.head(10))
+#Index(['sepal.length', 'sepal.width', 'petal.length', 'petal.width',
+#       'variety'],
 
 st.subheader("สถิติข้อมูลดอกไม้ Iris")
-
 st.write('ผลรวม')
+
+ 
